93e026be34d60a283c3ff0ef0ae8bb5c794d3f16
dahysl <41276441+dahysl@users.noreply.github.com>
2018-10-19 12:26:55 +0900

    ,

---
+++ VRAM.Data/VRAM.Gui/UpdateForm.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace VRAM.Gui
+{
+    partial class UpdateForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CtrlPanel5 = new System.Windows.Forms.Panel();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.CtrlPanel4 = new System.Windows.Forms.Panel();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lbEmail = new System.Windows.Forms.Label();
+            this.CtrlPanel3 = new System.Windows.Forms.Panel();
+            this.txtPhoneNum = new System.Windows.Forms.TextBox();
+            this.lbPhoneNum = new System.Windows.Forms.Label();
+            this.CtrlPanel2 = new System.Windows.Forms.Panel();
+            this.txtMemberName = new System.Windows.Forms.TextBox();
+            this.lbMemberName = new System.Windows.Forms.Label();
+            this.CtrlPanel = new System.Windows.Forms.Panel();
+            this.txtMemberId = new System.Windows.Forms.TextBox();
+            this.lbMemberId = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.CtrlPanel5.SuspendLayout();
+            this.CtrlPanel4.SuspendLayout();
+            this.CtrlPanel3.SuspendLayout();
+            this.CtrlPanel2.SuspendLayout();
+            this.CtrlPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CtrlPanel5
+            // 
+            this.CtrlPanel5.Controls.Add(this.txtPassword);
+            this.CtrlPanel5.Controls.Add(this.lbPassword);
+            this.CtrlPanel5.Location = new System.Drawing.Point(129, 314);
+            this.CtrlPanel5.Name = "CtrlPanel5";
+            this.CtrlPanel5.Size = new System.Drawing.Size(543, 48);
+            this.CtrlPanel5.TabIndex = 49;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(148, 13);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(394, 25);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Location = new System.Drawing.Point(3, 16);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(72, 15);
+            this.lbPassword.TabIndex = 0;
+            this.lbPassword.Text = "Password";
+            // 
+            // CtrlPanel4
+            // 
+            this.CtrlPanel4.Controls.Add(this.txtEmail);
+            this.CtrlPanel4.Controls.Add(this.lbEmail);
+            this.CtrlPanel4.Location = new System.Drawing.Point(129, 251);
+            this.CtrlPanel4.Name = "CtrlPanel4";
+            this.CtrlPanel4.Size = new System.Drawing.Size(543, 48);
+            this.CtrlPanel4.TabIndex = 50;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(148, 13);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(394, 25);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // lbEmail
+            // 
+            this.lbEmail.AutoSize = true;
+            this.lbEmail.Location = new System.Drawing.Point(3, 16);
+            this.lbEmail.Name = "lbEmail";
+            this.lbEmail.Size = new System.Drawing.Size(41, 15);
+            this.lbEmail.TabIndex = 0;
+            this.lbEmail.Text = "Email";
+            // 
+            // CtrlPanel3
+            // 
+            this.CtrlPanel3.Controls.Add(this.txtPhoneNum);
+            this.CtrlPanel3.Controls.Add(this.lbPhoneNum);
+            this.CtrlPanel3.Location = new System.Drawing.Point(129, 197);
+            this.CtrlPanel3.Name = "CtrlPanel3";
+            this.CtrlPanel3.Size = new System.Drawing.Size(543, 48);
+            this.CtrlPanel3.TabIndex = 51;
+            // 
+            // txtPhoneNum
+            // 
+            this.txtPhoneNum.Location = new System.Drawing.Point(148, 13);
+            this.txtPhoneNum.Name = "txtPhoneNum";
+            this.txtPhoneNum.Size = new System.Drawing.Size(394, 25);
+            this.txtPhoneNum.TabIndex = 1;
+            // 
+            // lbPhoneNum
+            // 
+            this.lbPhoneNum.AutoSize = true;
+            this.lbPhoneNum.Location = new System.Drawing.Point(3, 16);
+            this.lbPhoneNum.Name = "lbPhoneNum";
+            this.lbPhoneNum.Size = new System.Drawing.Size(78, 15);
+            this.lbPhoneNum.TabIndex = 0;
+            this.lbPhoneNum.Text = "Mobile No.";
+            // 
+            // CtrlPanel2
+            // 
+            this.CtrlPanel2.Controls.Add(this.txtMemberName);
+            this.CtrlPanel2.Controls.Add(this.lbMemberName);
+            this.CtrlPanel2.Location = new System.Drawing.Point(129, 143);
+            this.CtrlPanel2.Name = "CtrlPanel2";
+            this.CtrlPanel2.Size = new System.Drawing.Size(543, 48);
+            this.CtrlPanel2.TabIndex = 48;
+            // 
+            // txtMemberName
+            // 
+            this.txtMemberName.Location = new System.Drawing.Point(148, 13);
+            this.txtMemberName.Name = "txtMemberName";
+            this.txtMemberName.Size = new System.Drawing.Size(394, 25);
+            this.txtMemberName.TabIndex = 1;
+            // 
+            // lbMemberName
+            // 
+            this.lbMemberName.AutoSize = true;
+            this.lbMemberName.Location = new System.Drawing.Point(3, 16);
+            this.lbMemberName.Name = "lbMemberName";
+            this.lbMemberName.Size = new System.Drawing.Size(99, 15);
+            this.lbMemberName.TabIndex = 0;
+            this.lbMemberName.Text = "Member Name";
+            // 
+            // CtrlPanel
+            // 
+            this.CtrlPanel.Controls.Add(this.txtMemberId);
+            this.CtrlPanel.Controls.Add(this.lbMemberId);
+            this.CtrlPanel.Location = new System.Drawing.Point(129, 89);
+            this.CtrlPanel.Name = "CtrlPanel";
+            this.CtrlPanel.Size = new System.Drawing.Size(543, 48);
+            this.CtrlPanel.TabIndex = 47;
+            // 
+            // txtMemberId
+            // 
+            this.txtMemberId.Location = new System.Drawing.Point(148, 13);
+            this.txtMemberId.Name = "txtMemberId";
+            this.txtMemberId.Size = new System.Drawing.Size(394, 25);
+            this.txtMemberId.TabIndex = 1;
+            // 
+            // lbMemberId
+            // 
+            this.lbMemberId.AutoSize = true;
+            this.lbMemberId.Location = new System.Drawing.Point(3, 16);
+            this.lbMemberId.Name = "lbMemberId";
+            this.lbMemberId.Size = new System.Drawing.Size(71, 15);
+            this.lbMemberId.TabIndex = 0;
+            this.lbMemberId.Text = "MemberID";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(129, 378);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(205, 56);
+            this.btnUpdate.TabIndex = 48;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(467, 378);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(205, 56);
+            this.btnCancel.TabIndex = 52;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // UpdateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.CtrlPanel5);
+            this.Controls.Add(this.CtrlPanel4);
+            this.Controls.Add(this.CtrlPanel3);
+            this.Controls.Add(this.CtrlPanel2);
+            this.Controls.Add(this.CtrlPanel);
+            this.Name = "UpdateForm";
+            this.Text = "UpdateForm";
+            this.CtrlPanel5.ResumeLayout(false);
+            this.CtrlPanel5.PerformLayout();
+            this.CtrlPanel4.ResumeLayout(false);
+            this.CtrlPanel4.PerformLayout();
+            this.CtrlPanel3.ResumeLayout(false);
+            this.CtrlPanel3.PerformLayout();
+            this.CtrlPanel2.ResumeLayout(false);
+            this.CtrlPanel2.PerformLayout();
+            this.CtrlPanel.ResumeLayout(false);
+            this.CtrlPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel CtrlPanel5;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lbPassword;
+        private System.Windows.Forms.Panel CtrlPanel4;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lbEmail;
+        private System.Windows.Forms.Panel CtrlPanel3;
+        private System.Windows.Forms.TextBox txtPhoneNum;
+        private System.Windows.Forms.Label lbPhoneNum;
+        private System.Windows.Forms.Panel CtrlPanel2;
+        private System.Windows.Forms.TextBox txtMemberName;
+        private System.Windows.Forms.Label lbMemberName;
+        private System.Windows.Forms.Panel CtrlPanel;
+        private System.Windows.Forms.TextBox txtMemberId;
+        private System.Windows.Forms.Label lbMemberId;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
